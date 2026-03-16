6708a35a0be1c4614a4bc93731b2d4da79d795a7
Evaldas <evaldasweb10@gmail.com>
2026-03-16 11:53:55 +0200

    change files

--- kriziukai/settings.py
+++ kriziukai/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Application definition
